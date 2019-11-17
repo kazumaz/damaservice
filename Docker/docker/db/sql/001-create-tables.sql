d9ff4eeab18f8db7d7ad35deb1b5ba9a084dfd9d
kazuma <kazumazuma1124@gmail.com>
2019-11-17 22:09:50 +0900

    ユーザーTBLの追加

--- Docker/docker/db/sql/001-create-tables.sql
+++ Docker/docker/db/sql/001-create-tables.sql
@@ -1,5 +1,6 @@
 ---- drop ----
 DROP TABLE IF EXISTS `dama_table`;
+DROP TABLE IF EXISTS `user_table`;
 
 ---- create ----
 create table IF not exists `dama_table`
@@ -11,3 +12,11 @@ create table IF not exists `dama_table`
     PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+
+create table IF not exists `user_table`
+(
+ `id`               INT(20) AUTO_INCREMENT,
+ `name`             VARCHAR(20) NOT NULL,
+ `password`       VARCHAR(20) NOT NULL,
+    PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
