67bbd4d9c1911a178cd21f42af3eefe6ceb4e9a8
kazuma <kazumazuma1124@gmail.com>
2019-11-17 19:57:40 +0900

    first commit

---
+++ src/main/kotlin/com/dama/damaService/DamaServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.dama.damaService
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DamaServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<DamaServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/dama/damaService/DamaServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dama.damaService
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DamaServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
